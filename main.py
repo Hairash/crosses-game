dccf4e0a5ca756a83913be98b4fd3818fa7c6431
Hairash <maslevian@yandex.ru>
2022-04-10 00:44:18 +0400

    Add saves. Close #6

--- main.py
+++ main.py
@@ -2,14 +2,37 @@ from game import Game, EXIT_CODES
 
 WINDOW_SIZE = 600
 
-rows = 4
-difficulty_level = 1
-while True:
-    if difficulty_level > rows * rows:
-        rows += 1
-        difficulty_level = 1
-    game = Game(WINDOW_SIZE, rows, difficulty_level)
-    exit_code = game.start()
-    if exit_code == EXIT_CODES.QUIT:
-        break
-    difficulty_level += 1
+
+class GameCycle:
+    def __init__(self):
+        self.save_file_name = '.progress.sav'
+        self.rows = 4
+        self.difficulty_level = 1
+        self.load_game()
+
+    def load_game(self):
+        try:
+            with open(self.save_file_name, 'r') as f:
+                self.rows, self.difficulty_level = list(map(int, f.readline().split(':')))
+        except FileNotFoundError:
+            pass
+
+    def start(self):
+        while True:
+            if self.difficulty_level > self.rows * self.rows:
+                self.rows += 1
+                self.difficulty_level = 1
+            self.save_game()
+            game = Game(WINDOW_SIZE, self.rows, self.difficulty_level)
+            exit_code = game.start()
+            if exit_code == EXIT_CODES.QUIT:
+                return
+            self.difficulty_level += 1
+
+    def save_game(self):
+        with open(self.save_file_name, 'w') as f:
+            f.write(f'{self.rows}:{self.difficulty_level}')
+
+
+game_cycle = GameCycle()
+game_cycle.start()

3654f5f16d1b63be2569d2ea77ca30b900e25dc5
Hairash <maslevian@yandex.ru>
2022-04-10 00:10:49 +0400

    Change images

--- game.py
+++ game.py
@@ -6,14 +6,17 @@ cell_x, cell_y - cell coordinates (0..N-1)
 import pygame
 import random
 
+pygame.init()
 pygame.font.init()
-WHITE = (255, 255, 255)
-GRAY = (218, 218, 218)
 BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BACKGROUND = (34, 34, 34)
 
 
+# TODO: Make class label
 class WIN_LABEL:
     text = 'Congratulations!'
+    color = WHITE
     font = pygame.font.SysFont('Comic Sans', 48)
     width = 275
     height = 40
@@ -21,23 +24,22 @@ class WIN_LABEL:
 
 class STEP_COUNTER:
     text = 'Steps: '
+    color = WHITE
     font = pygame.font.SysFont('Comic Sans', 24)
     width = 100
     height = 20
 
 
 class CELL_VALUES:
-    SERVICE = -2
     EMPTY = -1
     HORIZONTAL = 0
     VERTICAL = 1
 
 
 CELL_VALUE_IMAGE_DICT = {
-    CELL_VALUES.SERVICE: 'images/black.png',
-    CELL_VALUES.EMPTY: 'images/white.png',
-    CELL_VALUES.HORIZONTAL: 'images/b_horizontal.png',
-    CELL_VALUES.VERTICAL: 'images/b_vertical.png',
+    CELL_VALUES.EMPTY: 'images/background.png',
+    CELL_VALUES.HORIZONTAL: 'images/croc_left.png',
+    CELL_VALUES.VERTICAL: 'images/croc_up.png',
 }
 
 
@@ -68,9 +70,7 @@ class Game:
         self.cur_step = 0
 
     def start(self):
-        pygame.init()
         pygame.display.set_caption(f'Crosses. Level: {self.difficulty_level}')
-
         self.draw_field()
         self.shuffle(self.difficulty_level)
 
@@ -99,11 +99,11 @@ class Game:
         self.increase_step()
 
     def change_cell_cross(self, cur_cell_x, cur_cell_y):
-        for cell_x in range(0, len(self.field)):
+        for cell_x in range(0, self.rows):
             if cell_x == cur_cell_x:
                 continue
             self.toggle_cell(cell_x, cur_cell_y)
-        for cell_y in range(0, len(self.field)):
+        for cell_y in range(0, self.rows):
             if cell_y == cur_cell_y:
                 continue
             self.toggle_cell(cur_cell_x, cell_y)
@@ -132,29 +132,32 @@ class Game:
         self.window.blit(self.images[value], (x, y))
 
     def draw_step_counter(self):
-        pygame.draw.rect(self.window, WHITE, (
+        pygame.draw.rect(self.window, BACKGROUND, (
             0, 0, self.window_size, STEP_COUNTER.height
         ))
         x = self.window_size - STEP_COUNTER.width
         y = 5
         text = STEP_COUNTER.text + str(self.cur_step)
-        text_img = STEP_COUNTER.font.render(text, True, BLACK)
+        text_img = STEP_COUNTER.font.render(text, True, STEP_COUNTER.color)
         self.window.blit(text_img, (x, y))
 
     def draw_field(self):
+        pygame.draw.rect(self.window, BACKGROUND, (
+            0, 0, self.window_size, self.window_size + STEP_COUNTER.height
+        ))
         self.draw_step_counter()
-        for x in range(len(self.field)):
+        for x in range(self.rows):
             for y in range(len(self.field[x])):
                 self.fill_cell(x, y)
 
     def shuffle(self, steps):
         cell_set = set()
         for _ in range(steps):
-            cell_x = random.randint(0, len(self.field) - 1)
-            cell_y = random.randint(0, len(self.field) - 1)
+            cell_x = random.randint(0, self.rows - 1)
+            cell_y = random.randint(0, self.rows - 1)
             while (cell_x, cell_y) in cell_set:
-                cell_x = random.randint(0, len(self.field) - 1)
-                cell_y = random.randint(0, len(self.field) - 1)
+                cell_x = random.randint(0, self.rows - 1)
+                cell_y = random.randint(0, self.rows - 1)
             self.change_cell_cross(cell_x, cell_y)
             cell_set.add((cell_x, cell_y))
 
@@ -167,6 +170,6 @@ class Game:
 
     def draw_win_label(self):
         x = int((self.window_size - WIN_LABEL.width) / 2)
-        y = int((self.window_size - WIN_LABEL.height) / 2)
-        text_img = WIN_LABEL.font.render(WIN_LABEL.text, True, BLACK)
+        y = int((self.window_size - WIN_LABEL.height) / 2) + STEP_COUNTER.height
+        text_img = WIN_LABEL.font.render(WIN_LABEL.text, True, WIN_LABEL.color)
         self.window.blit(text_img, (x, y))
